b637e4af7c1685417e7d5c49f656efb410f6cc2b
boooyeon <boooyeon@gmail.com>
2022-03-10 00:00:25 +0900

    로그인 1차 구현

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -10,7 +10,7 @@ import com.cos.blog.repository.UserRepository;
 
 // 스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 함(IoC를 해줌)
 // Service의 의미: 
-// Service가 필요한 이유1: 트랜잭션 관리
+// Service가 필요한 이유 1: 트랜잭션 관리
 @Service
 
 public class UserService {
@@ -23,7 +23,7 @@ public class UserService {
 		userRepository.save(user);
 	}
 
-	@Transactional(readOnly=true) // Select 할 때 트렌젝션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+	@Transactional(readOnly=true) // Select 할 때 트렌젝션 시 서비스 종료시에 트랜잭션 종료(정합성)
 	public User 로그인(User user) {
 		return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword()); //여러번하더라도 같은 데이터가 찾아짐
 	}
